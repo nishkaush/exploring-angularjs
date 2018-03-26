5e62b52d156e2daca5fb768ca1059c2db4a60326
nishkaush <kaushish.nishant@hotmail.com>
2018-03-26 17:21:53 +1100

    fixed up routing

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-var myAppModule = angular.module("myApp", []);
+var myAppModule = angular.module("myApp", ["ngRoute"]);
 
-myAppModule.config(function($routeProvider) {
+myAppModule.factory("firstFactory", function() {
+  var customers = [
+    {
+      name: "Pallav",
+      city: "Delhi"
+    },
+    {
+      name: "Sana",
+      city: "Sydney"
+    },
+    {
+      name: "Bush",
+      city: "Washington"
+    }
+  ];
+  var factory = {};
+  factory.getCustomers = function() {
+    return customers;
+  };
+  return factory;
+});
+
+myAppModule.controller("filteringNames", [
+  "$scope",
+  "firstFactory",
+  function($scope, firstFactory) {
+    $scope.customers = firstFactory.getCustomers();
+
+    $scope.addNewCustomer = function() {
+      // console.log($scope.newName);
+      // $scope.customers.push({ name: name, city: city });
+      $scope.customers.push({ name: $scope.newName, city: $scope.newCity });
+      $scope.newName = "";
+      $scope.newCity = "";
+    };
+  }
+]);
+
+myAppModule.config(function($routeProvider, $locationProvider) {
   $routeProvider
     .when("/", {
       controller: "filteringNames",
@@ -14,23 +52,3 @@ myAppModule.config(function($routeProvider) {
       redirectTo: "/"
     });
 });
-
-myAppModule.controller("filteringNames", [
-  "$scope",
-  function($scope) {
-    $scope.customers = [
-      {
-        name: "Pallav",
-        city: "Delhi"
-      },
-      {
-        name: "Sana",
-        city: "Sydney"
-      },
-      {
-        name: "Bush",
-        city: "Washington"
-      }
-    ];
-  }
-]);
